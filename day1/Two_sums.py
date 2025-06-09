bd48bb7b15d1e4162864ba48efef6c5b0dafd290
NYASHAMUKARAKATE <mukarakatenyasha34@gmail.com>
2025-06-09 20:40:20 +0200

    .

--- day1/Two_sums.py
+++ day1/Two_sums.py
@@ -2,16 +2,21 @@
 Given nums = [2, 7, 11, 15] and target = 9"""
 #using brute force method with O(n^2)
 #a nested loop to check pairs
-for i in range(len(nums)):  
-    for j in range(i + 1, len(nums)):  
-        if nums[i] + nums[j] == target:  
-            return [i, j]  
+nums = [2, 7, 11, 15] 
+target = 9
+
+def two_sum_brute_force(nums, target):
+    for i in range(len(nums)):  
+        for j in range(i + 1, len(nums)):  
+            if nums[i] + nums[j] == target:  
+                return [i, j]  
           
 #using optimal method O(n)
 #a dictionary to keep track of the seen numbers
-seen = {}  
-for i, num in enumerate(nums):  
-    complement = target - num  
-    if complement in seen:  
-        return [seen[complement], i]  
-    seen[num] = i  
+def two_sum_optimal(nums, target):
+    seen = {}  
+    for i, num in enumerate(nums):  
+        complement = target - num  
+        if complement in seen:  
+            return [seen[complement], i]  
+        seen[num] = i  
